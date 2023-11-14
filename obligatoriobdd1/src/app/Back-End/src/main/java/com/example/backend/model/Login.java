c225909bf5d04eb30263681270b74230b699f091
DiegoHanda <110860256+DiegoHanda@users.noreply.github.com>
2023-11-14 11:11:07 -0300

    Tablas

--- obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/model/Login.java
+++ obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/model/Login.java
@@ -5,21 +5,24 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Logins")
 public class Login {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "LogId")
   private int logId;
 
+  @Column(name = "admin")
+  private boolean admin;
+
   @Column(name = "password", nullable = false)
   private String password;
 
-  public Login(int logId, String password) {
-    this.logId = logId;
-    this.password = password;
+  public Login() {
   }
 
-  public Login() {
-
+  public Login(boolean admin, String password) {
+    this.admin = admin;
+    this.password = password;
   }
 
   public int getLogId() {
@@ -28,6 +31,14 @@ public class Login {
 
   public void setLogId(int logId) {
     this.logId = logId;
+  }
+
+  public boolean isAdmin() {
+    return admin;
+  }
+
+  public void setAdmin(boolean admin) {
+    this.admin = admin;
   }
 
   public String getPassword() {
