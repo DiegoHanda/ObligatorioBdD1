066e966669e1b7c9d282df77bcd567d48870af46
panchei19 <francisco.cabarcos@correo.ucu.edu.uy>
2023-11-15 23:09:24 -0300

    .

--- obligatoriobdd1/src/app/Front-End/carnet.service.ts
+++ obligatoriobdd1/src/app/Front-End/carnet.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Carnet} from './carnet';
+import { Carnet} from './Models/carnet';
 import { Observable, catchError } from 'rxjs';
 
 @Injectable({
