c2b428f826f5b0a6e4dc220856d247352a6ab2d0
DiegoHanda <110860256+DiegoHanda@users.noreply.github.com>
2023-11-16 18:35:56 -0300

    Crear Login Back

--- obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/controller/LoginController.java
+++ obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/controller/LoginController.java
@@ -2,11 +2,11 @@ package com.example.backend.controller;
 
 import com.example.backend.model.Login;
 import com.example.backend.repository.LoginRepository;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.Console;
 import java.util.List;
 
 @RestController
@@ -20,15 +20,25 @@ public class LoginController {
     return loginRepository.findAll();
   }
 
-
   @PostMapping("/login")
-  public boolean postLogin(@RequestBody Login login){
-    //System.out.println(login.getLogId());
-    String passwFromdb= loginRepository.findPasswordById(login.getLogId());
-    String inputPassw=login.getPassword();
-    //Login fromdb= new Login();
-    //return true;
-    return passwFromdb.equals(inputPassw);
+  public ResponseEntity<Login> crearLogin(@RequestBody Login login) {
+    if (loginRepository.existsByLogId(login.getLogId())) {
+      // If the login already exists, return a response indicating conflict
+      return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    // If the login does not exist, save and return the created login
+    Login createdLogin = loginRepository.save(login);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdLogin);
   }
+  /*@PostMapping("/login")
+  public boolean postLogin(@RequestBody Login login){
+    if (loginRepository.existsByLogId(login.getLogId())){
+      String passwFromDb = loginRepository.findPasswordById(login.getLogId());
+      String inputPassw = login.getPassword();
+      return inputPassw.equals(passwFromDb);
+    }
+    return false;
+  }*/
 }
 
