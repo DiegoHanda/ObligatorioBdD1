8134a965b2d27348c69defdb2500acccda4ef924
panchei19 <francisco.cabarcos@correo.ucu.edu.uy>
2023-11-17 08:20:34 -0300

    .

--- obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/repository/FuncionarioRepository.java
+++ obligatoriobdd1/src/app/Back-End/src/main/java/com/example/backend/repository/FuncionarioRepository.java
@@ -3,8 +3,10 @@ package com.example.backend.repository;
 import com.example.backend.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository <Funcionario, Integer>{
-
+@Query("SELECT f FROM Funcionario f WHERE f.ci NOT IN (SELECT ff.funcionario.ci FROM Agenda ff)")
+    List<Funcionario> findFuncionariosSinFormulario();
 }
