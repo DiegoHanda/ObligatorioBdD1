d309ed6cf54b50c607a704ce374ee162047d732a
panchei19 <francisco.cabarcos@correo.ucu.edu.uy>
2023-11-26 17:46:45 -0300

    .

--- obligatoriobdd1/src/app/Front-End/Services/agenda.service.ts
+++ obligatoriobdd1/src/app/Front-End/Services/agenda.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Agenda } from '../Models/agenda';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AgendaService {
-  private apiUrl = "http://localhost:3036/api/v1/agenda"; 
+  private apiUrl = "http://localhost:3036/api/v1/carnet_salud"; 
 
   constructor(private http: HttpClient) {}
 
-  getFechasDisponibles(): Observable<Agenda[]> {
-    return this.http.get<Agenda[]>(this.apiUrl);
+  getFechasDisponibles(): Observable<Date[]> {
+    const url = `${this.apiUrl}/agenda`;
+    return this.http.get<Date[]>(url);
   }
 }
