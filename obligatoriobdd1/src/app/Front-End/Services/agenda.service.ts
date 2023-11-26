81f94168ce403a7707af6bc02fbc07af6b4c6a4c
DiegoHanda <110860256+DiegoHanda@users.noreply.github.com>
2023-11-26 16:07:44 -0300

    pulleen

--- obligatoriobdd1/src/app/Front-End/Services/agenda.service.ts
+++ obligatoriobdd1/src/app/Front-End/Services/agenda.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Agenda } from '../Models/agenda';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AgendaService {
-  private apiUrl = "http://localhost:3036/api/v1/carnet_salud"; 
+  private apiUrl = "http://localhost:3036/api/v1/agenda"; 
 
   constructor(private http: HttpClient) {}
 
-  getFechasDisponibles(): Observable<Date[]> {
-    const url = `${this.apiUrl}/agenda`;
-    return this.http.get<Date[]>(url);
+  getFechasDisponibles(): Observable<Agenda[]> {
+    return this.http.get<Agenda[]>(this.apiUrl);
   }
 }
