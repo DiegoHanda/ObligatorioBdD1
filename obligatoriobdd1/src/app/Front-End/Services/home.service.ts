4480b2fd0a5795c3418f74963569e15cf4498551
DiegoHanda <110860256+DiegoHanda@users.noreply.github.com>
2023-11-27 11:47:00 -0300

    para pull

--- obligatoriobdd1/src/app/Front-End/Services/home.service.ts
+++ obligatoriobdd1/src/app/Front-End/Services/home.service.ts
@@ -8,52 +8,42 @@ import { HttpResponse } from '@angular/common/http';
   providedIn: 'root',
 })
 export class LogInServices {
-  private loginUrlLoginAdmin='http://localhost:3036/api/v1/loggear/Admin'
   private loginUrlCREARlogin = 'http://localhost:3036/api/v1/login';
   private loginUrlFuncionario = 'http://localhost:3036/api/v1/loggear';
   private loginUrlAdmin = 'http://localhost:3036/api/v1/adminlog';
 
   constructor(private http: HttpClient) {}
 
-   crearLogin(login:Login){
-    
-    return this.http.post<Login>(this.loginUrlCREARlogin, login)
-    .pipe(
-      catchError(error => {
-        console.error('Error:', error);
-        throw error;
-      })
-    );
-
-
-  }
-   deleteLogin(login:Login){
-   // alert(this.loginUrlCREARlogin+"/"+login.logId);
-    return this.http.delete(this.loginUrlCREARlogin+"/"+login.logId).pipe(
-      catchError((error) => {
-        alert("ERORR ELIMINAndo login");
-        console.error('Error:', error);
-        throw error;
-      })
-    );
-
-  }
-  submitLoginAdmin(Login: Login): Observable<object> {
-    return this.http.post<Login>(this.loginUrlLoginAdmin, Login).pipe(
+  crearLogin(login: Login) {
+    return this.http.post<Login>(this.loginUrlCREARlogin, login).pipe(
       catchError((error) => {
         console.error('Error:', error);
         throw error;
       })
     );
   }
-  
+  deleteLogin(login: Login) {
+    alert(this.loginUrlCREARlogin + '/' + login.logId);
+    return this.http.delete(this.loginUrlCREARlogin + '/' + login.logId).pipe(
+      //VERIFICAR SI SE CONCATENA BIEN LA STRING
+      catchError((error) => {
+        alert('ERORRELIMINA');
+        console.error('Error:', error);
+        throw error;
+      })
+    );
+  }
 
-  submitLoginFuncionario(Login: Login): Observable<object> {
+  submitLoginFuncionario(Login: Login): Observable<Object> {
     return this.http.post<Login>(this.loginUrlFuncionario, Login).pipe(
       catchError((error) => {
         console.error('Error:', error);
         throw error;
       })
     );
+  }
+
+  checkLogID(id: number): Observable<Object> {
+    return this.http.get(`${this.loginUrlCREARlogin}/exists/${id}`);
   }
 }
