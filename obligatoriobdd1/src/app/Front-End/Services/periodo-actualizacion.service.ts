44d03aa2a15472d7115aa7d9f216f59b93c1f44e
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2023-11-26 22:11:15 -0300

    periodo bloquea

--- obligatoriobdd1/src/app/Front-End/Services/periodo-actualizacion.service.ts
+++ obligatoriobdd1/src/app/Front-End/Services/periodo-actualizacion.service.ts
@@ -4,16 +4,99 @@ import { PeriodoActualizacion } from '../Models/periodoActualizacion';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PeriodoActualizacionService {
-  private apiUrl = "http://localhost:3036/api/v1/periodos_actualizacion";
+  private apiUrl = 'http://localhost:3036/api/v1/periodos_actualizacion';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   modificarPeriodo(pA: PeriodoActualizacion): Observable<Object> {
     console.log(pA);
     console.log(pA.fchFin);
     return this.http.put(this.apiUrl, pA);
   }
+  estamosEnPeriodo(): boolean {
+    let siEstamos: boolean = false;
+    /*  this.http.get(this.apiUrl).forEach(periodo => {
+      console.log(periodo)
+      alert(periodo);
+      
+    }); */
+
+    return siEstamos;
+  }
+
+  async estamosEnPeriodo2(): Promise<boolean> {
+    try {
+      const response: PeriodoActualizacion[] | undefined = await this.http
+        .get<PeriodoActualizacion[]>(this.apiUrl)
+        .toPromise();
+      //console.log('Response from server:', response);
+      if (response) {
+        let fechAct = this.convertirFecha(new Date().toDateString());
+        let fechFin: Date = response[0].fchFin;
+        let fechInicio: Date = response[0].fchInicio;
+        let fechInicioSTR: string = fechInicio.toString();
+        let fechActSTR: string = fechAct.toString();
+        let fechFinSTR: string = fechFin.toString();
+
+        console.log('ACTUAl:', fechAct);
+        console.log('INICIO:', fechInicio);
+        console.log('FIN:', fechFin);
+
+        let esMayorqueFINICIO: boolean = this.compararFechas(
+          fechActSTR,
+          fechInicioSTR
+        );
+        let esMenorqueFINICIO: boolean = this.compararFechas(
+          fechFinSTR,
+          fechActSTR
+        );
+        console.log(esMayorqueFINICIO, esMenorqueFINICIO);
+        return (esMayorqueFINICIO && esMenorqueFINICIO);
+      }
+    } catch (error) {}
+
+    return false;
+  }
+
+  convertirFecha(fecha: string): string {
+    const fechaObjeto = new Date(fecha);
+
+    const year = fechaObjeto.getFullYear();
+    const month = ('0' + (fechaObjeto.getMonth() + 1)).slice(-2); // Agrega un 0 al mes si es necesario
+    const day = ('0' + fechaObjeto.getDate()).slice(-2); // Agrega un 0 al día si es necesario
+
+    return `${year}-${month}-${day}`;
+  }
+
+  compararFechas(fechaStr1: string, fechaStr2: string): boolean {
+    const fecha1 = new Date(fechaStr1);
+    const fecha2 = new Date(fechaStr2);
+
+    /* if (isNaN(fecha1.getTime()) || isNaN(fecha2.getTime())) {
+      // Verifica si las fechas son inválidas
+      return null; // Devuelve null si alguna de las fechas es inválida
+    } */
+
+    if (fecha1 > fecha2) {
+      return true;
+    } else if (fecha1 < fecha2) {
+      return false;
+    } else {
+      return false;
+    }
+  }
+
+  /* // Ejemplo de uso
+  const fechaA = '2023-11-26';
+  const fechaB = '2023-11-30';
+  
+  const resultado = compararFechas(fechaA, fechaB);
+  if (resultado) {
+    console.log(resultado);
+  } else {
+    console.error('Formato de fecha inválido');
+  } */
 }
